3986cf05eb917534df35c4f820a7fb21a2c25099
Stewardvr93 <91085277+Stewardvr93@users.noreply.github.com>
2022-08-30 21:07:28 -0500

    Task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,22 +1,14 @@
 #!/usr/bin/python3
-# script that takes in an argument and displays all values in the states
-# table of hbtn_0e_0_usa where name matches the argument
+"""
+Displays all values in the states table of the database hbtn_0e_0_usa
+"""
+import sys
 import MySQLdb
-from sys import argv
-
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2],
-                           db=argv[3], port=3306)
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name = '{:s}' \
-                 COLLATE latin1_general_cs ORDER BY id ASC".format(argv[4]))
-
-    query_rows = cur.fetchall()
-
-    for row in query_rows:
-        print(row)
-    # Close all cursors
-    cur.close()
-    # Close all databases
-    conn.close()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * \
+                 FROM `states` \
+                WHERE BINARY `name` = '{}'".format(sys.argv[4]))
+    [print(state) for state in c.fetchall()]
