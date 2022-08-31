59684714d36f478e7b74fdd5421da8ba73d48b25
Stewardvr93 <91085277+Stewardvr93@users.noreply.github.com>
2022-08-30 21:10:27 -0500

    Task 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,30 +1,17 @@
 #!/usr/bin/python3
-"""script that takes in the name of a state as an argument and lists all cities
-   of that state, using the database hbtn_0e_4_usa
 """
+Displays all cities of a given state from the
+states table of the database hbtn_0e_4_usa.
+Safe from SQL injections.
+"""
+import sys
 import MySQLdb
-from sys import argv
-
 
 if __name__ == "__main__":
-    aux_list = []
-    conn = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2],
-                           db=argv[3], port=3306)
-    cur = conn.cursor()
-    # , to the end of argv[4] because second paramether is a tuple
-    cur.execute("SELECT c.name \
-                 FROM cities as c \
-                 INNER JOIN states as s \
-                 ON c.state_id = s.id \
-                 WHERE s.name = %s \
-                 COLLATE latin1_general_cs \
-                 ORDER BY c.id ASC;", (argv[4],))
-    query_rows = cur.fetchall()
-
-    for row in query_rows:
-        aux_list.append(row[0])
-    print(", ".join(aux_list))
-    # Close all cursors
-    cur.close()
-    # Close all databases
-    conn.close()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `cities` as `c` \
+                INNER JOIN `states` as `s` \
+                   ON `c`.`state_id` = `s`.`id` \
+                ORDER BY `c`.`id`")
+    print(", ".join([ct[2] for ct in c.fetchall() if ct[4] == sys.argv[4]]))
