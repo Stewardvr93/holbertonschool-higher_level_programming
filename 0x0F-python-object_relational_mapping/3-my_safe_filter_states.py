efcf6f81c6511373eab7f3c0c3d1745799b9458f
Stewardvr93 <91085277+Stewardvr93@users.noreply.github.com>
2022-08-30 21:08:22 -0500

    Task 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,23 +1,14 @@
 #!/usr/bin/python3
-"""AVOID SQL INJECTIONS. script that takes in an argument and displays all
-values in the states table of hbtn_0e_0_usa where name matches the argument
 """
+Displays all values in the states table of the database hbtn_0e_0_usa
+whose name matches that supplied as argument.
+Safe from SQL injections.
+"""
+import sys
 import MySQLdb
-from sys import argv
-
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2],
-                           db=argv[3], port=3306, charset="utf8")
-    cur = conn.cursor()
-    # , to the end of argv[4] because second paramether is a tuple
-    cur.execute("SELECT * FROM states WHERE name = %s \
-                 ORDER BY id ASC", (argv[4],))
-    query_rows = cur.fetchall()
-
-    for row in query_rows:
-        print(row)
-    # Close all cursors
-    cur.close()
-    # Close all databases
-    conn.close()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states`")
+    [print(state) for state in c.fetchall() if state[1] == sys.argv[4]]
